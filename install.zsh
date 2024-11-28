5c194a457c2d18b2f7068bd98e9cd80119591f43
yaito6502 <schwarzekatze8010110@gmail.com>
2024-11-29 06:10:14 +0900

    fix: set the homebrew path.

--- install.zsh
+++ install.zsh
@@ -1,13 +1,25 @@
 #!/bin/zsh
 
+user_name=$(whoami)
+
+#xcode command line
 xcode-select --install
 
 #homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/$user_name/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+if ! command -v brew &> /dev/null
+then
+    echo "brewのインストールに失敗したか、パスが通っていません"
+    exit 1
+fi
 
 #ansible
 brew install ansible
 
+#execute playbook
 cd ansible
 cp inventory.ini.example inventory.ini
 user_name=$(whoami)
